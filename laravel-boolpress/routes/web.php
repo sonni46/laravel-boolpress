877ac19db6a88b97d5bdaab709626064753cfa41
Marco <sonni46marcosenpai@gmail.com>
2021-11-04 17:29:49 +0100

    init

---
+++ laravel-boolpress/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')
+->group(function(){
+    //pagina di atterraggio dopo il login (con il prefix, l'url è /admin)
+    Route::get('/', 'HomeController@index')->name('index');
+});
